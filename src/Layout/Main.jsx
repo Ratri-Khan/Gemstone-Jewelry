4f0e05274426be2345ab1de89afabb22b5a50d5a
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-10-07 12:26:18 +0600

    header part complete

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+import Header from "../Component/Header/Header";
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Main;
