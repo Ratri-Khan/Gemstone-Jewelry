e8d49957d1a2d91e183ead3db4d67b42f542d8a1
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-10-08 20:17:03 +0600

    footer complete

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,26 +3,28 @@ import Main from "../Layout/Main";
 import Home from "../Component/Home/Home";
 import AddJewelry from "../Component/AddJewelry/AddJewelry";
 import AllJewelry from "../Component/AllJewelry/AllJewelry";
+import Footer from "../Component/Footer/Footer";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>,
-        },
-        {
-            path: "/addJewelry",
-            element: <AddJewelry></AddJewelry>,
-        },
-        {
-          path: "/allJewelry",
-          element: <AllJewelry></AllJewelry>,
-          loader: () =>fetch('http://localhost:3000/jewelry')
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
       },
-      ],
-    },
-  ]);
-  export default router;
+      {
+        path: "/addJewelry",
+        element: <AddJewelry></AddJewelry>,
+      },
+      {
+        path: "/allJewelry",
+        element: <AllJewelry></AllJewelry>,
+        loader: () => fetch('http://localhost:3000/jewelry')
+      },
+      
+    ],
+  },
+]);
+export default router;
